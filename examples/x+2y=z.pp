dbee1396cfea39f17fe12106a4f4275437e571fb
Yoan Rougeolle <yoyo031201@orange.fr>
2022-03-05 14:07:14 +0100

    Correction du x + 2y = z

--- examples/x+2y=z.pp
+++ examples/x+2y=z.pp
@@ -1,20 +1,19 @@
 Indique si x + 2y = z
 TITLE: x + 2y = z
-STATES: x y z xy zz zzz 0 1
+STATES: x y z xx zz 0 1
 INITIAL: x y z
 YES: 1
-NO: x y z xy zz zzz 0
+NO: x y z xx zz 0
 RULES:
-    x y -> xy 0
+    x z -> 1 0
     z z -> zz 0
-    zz z -> zzz 0
-    zz zz -> zzz z
-    xy zzz -> 1 1
+    zz y -> 1 0
+    x x -> xx 0
+    xx zz -> 1 0
     x 1 -> x 0
     y 1 -> y 0
     z 1 -> z 0
-    xy 1 -> xy 0
     zz 1 -> zz 0
-    zzz 1 -> zzz 0
     1 0 -> 1 1
 CONF: x=2 y=2 z=6
+CONF: x=2 y=1 z=4
